67901a8b1c9c32922bb0b6bbdf681177b035d710
Justin Richards <richards.justind@gmail.com>
2017-01-13 16:27:26 -0600

    adding cors module

--- server.js
+++ server.js
@@ -77,7 +77,6 @@ app.use(appMiddleWare.errorHandler);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
